fc95199a8e6e6ac51cbec4c55de516b39b2785f8
silva-mateus <46615092+silva-mateus@users.noreply.github.com>
2020-03-11 13:57:39 -0300

    Final Changes

--- the-rosa/js/main.js
+++ the-rosa/js/main.js
@@ -1,1 +1,32 @@
-//Select element function
+$(".menu-toggle").on("click", function() {
+  $("body").toggleClass("open");
+});
+
+// Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300
+});
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
